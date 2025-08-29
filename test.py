22b6dc8b26c6c1872e9e958c7854be6f73cc66f3
Piotr Goworek <56643198+Gowi99@users.noreply.github.com>
2025-08-29 16:48:18 +0200

    Update main.py and test.py files

--- test.py
+++ test.py
@@ -36,8 +36,28 @@ def get_match_details(match_id, PLATFORM, API_KEY):
         print(f"Error: {response.status_code}, {response.text}")
         return None
 
+def get_gameName_and_tagLine_from_puuid(puuid, PLATFORM, API_KEY):
+    url = f"https://{PLATFORM}.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}"
+    headers = {"X-Riot-Token": API_KEY}
+    response = httpx.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("gameName"), data.get("tagLine")
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None, None
+
 puuid = get_puuid(gameName, tagLine, PLATFORM, API_KEY)
 print(f"PUUID: {puuid}")
 
 match_history = get_match_history(get_puuid(gameName, tagLine, PLATFORM, API_KEY), PLATFORM, API_KEY)
 print(match_history)
+
+praticipants = get_match_details(match_history[0], PLATFORM, API_KEY)['metadata']['participants']
+print(len(praticipants))
+
+for puuid in praticipants:
+    gameName, tagLine = get_gameName_and_tagLine_from_puuid(puuid, PLATFORM, API_KEY)
+    print(f"{gameName}#{tagLine}")
+
+
