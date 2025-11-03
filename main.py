b59477c2920fa08303c076a05adbde9613b72abd
Gowi <piotrgoworek99@gmail.com>
2025-11-03 20:07:25 +0100

    Add winrate in main.py and templates

--- main.py
+++ main.py
@@ -6,7 +6,8 @@
 # mozna zrobic wykres winratio na przestrzeni np. tygodnia, miesiaca, roku
 # mozna zrobic wybor z listy bohaterow a pozniej winratio dzienne z tygodnia
 import logging
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_snake
 from config import API_KEY
 from fastapi import FastAPI, Form, Request, HTTPException
 from fastapi.responses import HTMLResponse
@@ -17,8 +18,22 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# PLATFORM = "EUROPE" 
-# REGION = "EUN1"
+class ParticipantDto(BaseModel):
+    model_config = ConfigDict(alias_generator=to_snake, populate_by_name=True, extra='ignore')
+    puuid: str
+    win: bool
+
+class InfoDto(BaseModel):
+    model_config = ConfigDict(alias_generator=to_snake, populate_by_name=True, extra='ignore')
+    participants: list[ParticipantDto]
+
+class MetadataDto(BaseModel):
+    matchId: str
+
+class MatchDto(BaseModel):
+    model_config = ConfigDict(alias_generator=to_snake, populate_by_name=True, extra='ignore')
+    info: InfoDto
+
 
 def get_puuid(gameName, tagLine, PLATFORM, API_KEY):
     url = f"https://{PLATFORM}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}"
@@ -35,26 +50,17 @@ def get_puuid(gameName, tagLine, PLATFORM, API_KEY):
     else:
         return f"Error: {response.status_code}, {response.text}"
 
-
-def get_last_100_matches_by_puuid(puuid: str, PLATFORM: str, API_KEY: str) -> list:
+def get_last_100_matches_by_puuid(puuid: str, PLATFORM: str, API_KEY: str) -> list | None:
     url = f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=100"
     headers = {"X-Riot-Token": API_KEY}
     response = httpx.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Error: {response.status_code}, {response.text}")
+        logging.info(f"Error: {response.status_code}, {response.text}")
         return None
 
-
-# sprawdzic zwyciestwo dla kazdego meczu i zwrocic %
-def winrate(list_of_matches):   
-    pass
-
-
-# jedyne zagrożenie to zagranie tego samego meczu w przeciwnych drużynach 
-# (liczy jako wspolny mecz, mozna wykluczyc sprawdzajac czy puuid1 i puuid2 sa w participants albo winratio dla kazdego)
-def last_10_common_matches_winrate(puuid1: str, puuid2: str, PLATFORM: str, API_KEY: str) -> list:
+def last_10_common_matches(puuid1: str, puuid2: str, PLATFORM: str, API_KEY: str) -> list:
     match_history1 = get_last_100_matches_by_puuid(puuid1, PLATFORM, API_KEY)
     match_history2 = get_last_100_matches_by_puuid(puuid2, PLATFORM, API_KEY)
     i = 0
@@ -65,8 +71,39 @@ def last_10_common_matches_winrate(puuid1: str, puuid2: str, PLATFORM: str, API_
             i += 1
         if i == 10:
             return common_matches[:10]
-    return common_matches       
-    
+    return common_matches     
+
+def get_if_common_win(puuid1: str, puuid2: str, match_id: str, PLATFORM: str, API_KEY: str) -> bool | None:
+    url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
+    headers = {"X-Riot-Token": API_KEY}
+    response = httpx.get(url, headers=headers)
+    if response.status_code == 200:
+        puuid1_win = False
+        puuid2_win = False
+        match = MatchDto(**response.json())
+        logging.info(type(match.info.participants))
+        for player in match.info.participants:
+            if player.puuid == puuid1 and player.win:
+                puuid1_win = True
+            elif player.puuid == puuid2 and player.win:
+                puuid2_win = True
+        if puuid1_win and puuid2_win:
+            return True
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
+
+def get_winrate(puuid1: str, puuid2: str, platform: str, API_KEY: str) -> float:
+    last_10 = last_10_common_matches(puuid1, puuid2, platform, API_KEY)
+    if last_10:
+        common_wins = 0
+        for match_id in last_10:
+            if get_if_common_win(puuid1, puuid2, match_id, platform, API_KEY):
+                common_wins += 1
+        winrate = common_wins / len(last_10) * 100
+        return winrate
+    else:
+        return None
 
 def region_to_platform(region: str) -> str:
     mapping = {
@@ -84,7 +121,6 @@ def region_to_platform(region: str) -> str:
     }
     return mapping.get(region.lower(), "EUROPE")
 
-
 # Strona z formularzem
 @app.get("/", response_class=HTMLResponse)
 async def form_page(request: Request):
@@ -98,18 +134,19 @@ async def get_puuid_route(request: Request, gameName1: str = Form(...), tagLine1
         raise HTTPException(status_code=400, detail=f"Nieznany region: {region}")
     if not platform2:
         raise HTTPException(status_code=400, detail=f"Nieznany region: {region}")
+    if platform1 != platform2:
+        # pytanie czy to dziala
+        return templates.TemplateResponse("form_2_players.html", {"request": request, "error": f"Gracze musza byc z tego samego regionu", "puuid1": puuid1, "puuid2": puuid2})
     puuid1 = get_puuid(gameName1, tagLine1, platform1, API_KEY)
     puuid2 = get_puuid(gameName2, tagLine2, platform2, API_KEY)
-    winrate = None  # Tu bedzie trzeba dorzucic winrate
+    winrate = get_winrate(puuid1, puuid2, platform1, API_KEY)
+    # tutaj trzeba zrobic ifa dla roznych wynikow winratio: nie bylo wspolnych, bylo mniej niz 10, bylo 10
+    # jak nie bedzie wspolnych meczy to zwroci None i trzeba ta wartość obsluzyc itp.
     if puuid1.startswith("Error") or puuid2.startswith("Error"):
         return templates.TemplateResponse("form_2_players.html", {"request": request, "error": f"Błędne dane", "puuid1": puuid1, "puuid2": puuid2})
     return templates.TemplateResponse("result.html", {"request": request, "gameName1": gameName1,"tagLine1": tagLine1, "gameName2": gameName2,"tagLine2": tagLine2, "puuid1": puuid1, "puuid2": puuid2, "winrate": winrate})
 
-# @app.get("/last10gameswinrate", response_class=HTMLResponse)
-# async def last10gameswinrate(request: Request):
-#     return templates.TemplateResponse("result.html", {"request": request})  # Tu bedzie trzeba dorzucic winrate
-
-@app.post("/test", response_class=HTMLResponse)
-async def test(request: Request, gameName: str = Form(...), tagLine: str = Form(...), region: str = Form(...)):
-    return templates.TemplateResponse("test.html", {"request": request, "gameName": gameName, "tagLine": tagLine, "region": region})
+@app.get("/last10gameswinrate", response_class=HTMLResponse)
+async def last10gameswinrate(request: Request):
+    return templates.TemplateResponse("result.html", {"request": request, "gameName1": gameName1,"tagLine1": tagLine1, "gameName2": gameName2,"tagLine2": tagLine2, "puuid1": puuid1, "puuid2": puuid2, "winrate": winrate})
 
--- test.py
+++ test.py
@@ -1,12 +1,44 @@
 from config import API_KEY
 import httpx
 from datetime import datetime, timedelta
-
+from pydantic import BaseModel
+import json
 
 PLATFORM = "EUROPE" 
 REGION = "EUN1"
 gameName = "Gowi"
 tagLine = "420"
+
+class TeamDto(BaseModel):
+    teamId: int | None = None
+    win: bool | None = None
+
+class ParticipantDto(BaseModel):
+    teamId : int | None = None
+
+class MetadataDto(BaseModel):
+    pass
+
+class InfoDto(BaseModel):
+    gameCreation: int | None = None
+    gameDuration: int | None = None
+    gameEndTimestamp: int | None = None
+    gameId: int
+    gameMode: str | None = None
+    gameName: str | None = None
+    gameType: str | None = None
+    gameVersion: str | None = None
+    mapId: int | None = None
+    participants: list[ParticipantDto]
+    platformId: str | None = None
+    queueId: int | None = None
+    teams: list[TeamDto] | None = None
+    tournamentCode: str | None = None
+
+class MatchDto(BaseModel):
+    metadata: MetadataDto
+    info: InfoDto
+
 
 def get_puuid(gameName, tagLine, PLATFORM, API_KEY):
     url = f"https://{PLATFORM}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}"
@@ -33,104 +65,106 @@ def get_match_details(match_id, PLATFORM, API_KEY):
     headers = {"X-Riot-Token": API_KEY}
     response = httpx.get(url, headers=headers)
     if response.status_code == 200:
-        return response.json()  # pytanie czy my chcemy całego jsona czy tylko jakąś jego część, bo szkoda zapytań
+        match = MatchDto(**response.json())
+        return match.info
     else:
         print(f"Error: {response.status_code}, {response.text}")
         return None
 
-def get_gameName_and_tagLine_from_puuid(puuid, PLATFORM, API_KEY):
-    url = f"https://{PLATFORM}.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}"
-    headers = {"X-Riot-Token": API_KEY}
-    response = httpx.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("gameName"), data.get("tagLine")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return None, None
+# def get_gameName_and_tagLine_from_puuid(puuid, PLATFORM, API_KEY):
+#     url = f"https://{PLATFORM}.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}"
+#     headers = {"X-Riot-Token": API_KEY}
+#     response = httpx.get(url, headers=headers)
+#     if response.status_code == 200:
+#         data = response.json()
+#         return data.get("gameName"), data.get("tagLine")
+#     else:
+#         print(f"Error: {response.status_code}, {response.text}")
+#         return None, None
+
+# def get_gameStartTimestamp(match_id, PLATFORM, API_KEY):
+#     url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
+#     headers = {"X-Riot-Token": API_KEY}
+#     response = httpx.get(url, headers=headers)
+#     if response.status_code == 200:
+#         data = response.json()
+#         return data.get("info", {}).get("gameStartTimestamp")
+#     else:
+#         print(f"Error: {response.status_code}, {response.text}")
+#         return None, None
+    
+# def get_gameEndTimestamp(match_id, PLATFORM, API_KEY):
+#     url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
+#     headers = {"X-Riot-Token": API_KEY}
+#     response = httpx.get(url, headers=headers)
+#     if response.status_code == 200:
+#         data = response.json()
+#         return data.get("info", {}).get("gameEndTimestamp")
+#     else:
+#         print(f"Error: {response.status_code}, {response.text}")
+#         return None, None
+    
+# def get_endOfGameResult(match_id, PLATFORM, API_KEY):
+#     url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
+#     headers = {"X-Riot-Token": API_KEY}
+#     response = httpx.get(url, headers=headers)
+#     if response.status_code == 200:
+#         data = response.json()
+#         return data.get("info", {}).get("endOfGameResult", {})
+#     else:
+#         print(f"Error: {response.status_code}, {response.text}")
+#         return None, None
+
+# def get_gameDuration(match_id, PLATFORM, API_KEY):
+#     url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
+#     headers = {"X-Riot-Token": API_KEY}
+#     response = httpx.get(url, headers=headers)
+#     if response.status_code == 200:
+#         data = response.json()
+#         return data.get("info", {}).get("gameDuration")
+#     else:
+#         print(f"Error: {response.status_code}, {response.text}")
+#         return None, None
+
+# def get_gameCreation(match_id, PLATFORM, API_KEY):
+#     url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
+#     headers = {"X-Riot-Token": API_KEY}
+#     response = httpx.get(url, headers=headers)
+#     if response.status_code == 200:
+#         data = response.json()
+#         return data.get("info", {}).get("gameCreation")
+#     else:
+#         print(f"Error: {response.status_code}, {response.text}")
+#         return None, None
+
 
 puuid = get_puuid(gameName, tagLine, PLATFORM, API_KEY)
-print(f"PUUID: {puuid}")
+# print(f"PUUID: {puuid}")
 
 match_history = get_match_history(get_puuid(gameName, tagLine, PLATFORM, API_KEY), PLATFORM, API_KEY)
-print(match_history)
+# print(match_history)
 
-# Pobieranie zawodników z historii meczów
+x = get_match_details(match_history[0], PLATFORM, API_KEY)
+print(x.participants)
 
-praticipants = get_match_details(match_history[0], PLATFORM, API_KEY)['metadata']['participants']
-print(len(praticipants))
+# # Pobieranie zawodników z historii meczów
 
-for puuid in praticipants:
-    gameName, tagLine = get_gameName_and_tagLine_from_puuid(puuid, PLATFORM, API_KEY)
-    print(f"{gameName}#{tagLine}")
+# praticipants = get_match_details(match_history[0], PLATFORM, API_KEY)['metadata']['participants']
+# print(len(praticipants))
 
-def get_gameStartTimestamp(match_id, PLATFORM, API_KEY):
-    url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
-    headers = {"X-Riot-Token": API_KEY}
-    response = httpx.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("info", {}).get("gameStartTimestamp")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return None, None
-    
-start_time = datetime.fromtimestamp(get_gameStartTimestamp(match_history[0], PLATFORM, API_KEY)/1000)
-print(f"Start time: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
+# for puuid in praticipants:
+#     gameName, tagLine = get_gameName_and_tagLine_from_puuid(puuid, PLATFORM, API_KEY)
+#     print(f"{gameName}#{tagLine}")
 
+# start_time = datetime.fromtimestamp(get_gameStartTimestamp(match_history[0], PLATFORM, API_KEY)/1000)
+# print(f"Start time: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-def get_gameEndTimestamp(match_id, PLATFORM, API_KEY):
-    url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
-    headers = {"X-Riot-Token": API_KEY}
-    response = httpx.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("info", {}).get("gameEndTimestamp")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return None, None
-    
-end_time = datetime.fromtimestamp(get_gameEndTimestamp(match_history[0], PLATFORM, API_KEY)/1000)
-print(f"End time: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+# print(get_endOfGameResult(match_history[0], PLATFORM, API_KEY))
 
+# end_time = datetime.fromtimestamp(get_gameEndTimestamp(match_history[0], PLATFORM, API_KEY)/1000)
+# print(f"End time: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-def get_endOfGameResult(match_id, PLATFORM, API_KEY):
-    url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
-    headers = {"X-Riot-Token": API_KEY}
-    response = httpx.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("info", {}).get("endOfGameResult")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return None, None
+# # sposob liczenia daty meczu dla op.gg
+# print(datetime.fromtimestamp(get_gameCreation(match_history[1], PLATFORM, API_KEY)/1000) + timedelta(seconds=get_gameDuration(match_history[1], PLATFORM, API_KEY)))
 
-print(get_endOfGameResult(match_history[0], PLATFORM, API_KEY))
-
-def get_gameDuration(match_id, PLATFORM, API_KEY):
-    url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
-    headers = {"X-Riot-Token": API_KEY}
-    response = httpx.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("info", {}).get("gameDuration")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return None, None
-
-def get_gameCreation(match_id, PLATFORM, API_KEY):
-    url = f"https://{PLATFORM}.api.riotgames.com/lol/match/v5/matches/{match_id}"
-    headers = {"X-Riot-Token": API_KEY}
-    response = httpx.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("info", {}).get("gameCreation")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return None, None
-
-
-# sposob liczenia daty meczu dla op.gg
-print(datetime.fromtimestamp(get_gameCreation(match_history[1], PLATFORM, API_KEY)/1000) + timedelta(seconds=get_gameDuration(match_history[1], PLATFORM, API_KEY)))
-
-print(len(get_match_history(get_puuid(gameName, tagLine, PLATFORM, API_KEY), PLATFORM, API_KEY)))
+# print(len(get_match_history(get_puuid(gameName, tagLine, PLATFORM, API_KEY), PLATFORM, API_KEY)))
